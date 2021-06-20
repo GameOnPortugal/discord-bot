f4b6294d83a4c5b63e90e395c24cd12b24835a69
Jose Lopes <josluis.lopes@gmail.com>
2021-06-20 13:36:03 +0100

    Added migration and model

---
+++ src/migrations/20210620123126-create-special-channel.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('SpecialChannels', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      channelId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      specialType: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      data: {
+        allowNull: true,
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('SpecialChannels');
+  }
+};
---
+++ src/models/specialchannel.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class SpecialChannel extends Model {
+    static associate(models) {
+    }
+  };
+
+  SpecialChannel.init(
+      {
+        channelId: DataTypes.INTEGER,
+        specialType: DataTypes.STRING,
+        data: DataTypes.TEXT
+      },
+      {
+        sequelize,
+        modelName: 'SpecialChannel',
+      }
+      );
+
+  return SpecialChannel;
+};
