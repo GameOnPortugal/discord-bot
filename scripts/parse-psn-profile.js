4bd5ee61ec185682be46928d2dbd9f71ea12ca71
Jose Lopes <josluis.lopes@gmail.com>
2021-10-08 16:27:20 +0100

    Optimise parse-psn-profile so it doesn't consume the world resources

--- scripts/parse-psn-profile.js
+++ scripts/parse-psn-profile.js
@@ -6,6 +6,32 @@ const TrophiesManager = require('./../src/service/trophy/trophyManager');
 const { Webhook } = require('discord-webhook-node');
 const hook = new Webhook(process.env.TROPHY_WEBHOOK);
 
+async function createTrophies(trophyProfile, urls) {
+	for (const trophyUrl of urls) {
+		let trophy = await TrophiesManager.findByUsernameAndUrl(trophyProfile, trophyUrl);
+		if (trophy) {
+			console.log('Trophy ' + trophyUrl + ' is already submitted!');
+			console.log('Stopping going through the urls as i think I\'ve catched up');
+
+			return;
+		}
+
+		try {
+			const trophyData = await PsnCrawlService.getPlatTrophyData(trophyUrl);
+			trophy = await TrophiesManager.create(trophyProfile, trophyUrl, trophyData);
+		}
+		catch (exception) {
+			console.error(exception);
+
+			continue;
+		}
+
+		console.log('Trophy created successfully. Sending webhook!');
+
+		hook.send('Parabéns <@' + trophyProfile.userId + '>! Acabaste de receber ' + trophy.points + ' TP (Trophy Points) pelo teu troféu: ' + trophyUrl);
+	}
+}
+
 (async () => {
 	const trophyProfiles = await TrophyProfileManager.findAll();
 
@@ -25,28 +51,7 @@ const hook = new Webhook(process.env.TROPHY_WEBHOOK);
 		const urls = await PsnCrawlService.getProfileTrophies(trophyProfile.psnProfile);
 		console.log('Found ' + urls.length + ' trophy urls for this profile.');
 
-		for (const trophyUrl of urls) {
-			let trophy = await TrophiesManager.findByUsernameAndUrl(trophyProfile, trophyUrl);
-			if (trophy) {
-				console.log('Trophy ' + trophyUrl + ' is already submitted!');
-
-				continue;
-			}
-
-			try {
-				const trophyData = await PsnCrawlService.getPlatTrophyData(trophyUrl);
-				trophy = await TrophiesManager.create(trophyProfile, trophyUrl, trophyData);
-			}
-			catch (exception) {
-				console.error(exception);
-
-				continue;
-			}
-
-			console.log('Trophy created successfully. Sending webhook!');
-
-			hook.send('Parabéns <@' + trophyProfile.userId + '>! Acabaste de receber ' + trophy.points + ' TP (Trophy Points) pelo teu troféu: ' + trophyUrl);
-		}
+		await createTrophies(trophyProfile, urls);
 
 		console.log(' <<< Job ended <<<<');
 	}
