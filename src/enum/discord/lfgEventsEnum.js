644cb600c489ebe9df2f43654cd0961e194493f4
Jose Lopes <josluis.lopes@gmail.com>
2022-08-04 10:38:57 +0100

    Change event points

--- src/enum/discord/lfgEventsEnum.js
+++ src/enum/discord/lfgEventsEnum.js
@@ -7,14 +7,14 @@ module.exports = {
 		These points need revising.
 	*/
 	LFG_EVENTS: {
-		game_create: lfgevent('game_create', 10),
-		game_cancel: lfgevent('game_cancel', -10),
-		commendation: lfgevent('commendation', 10),
+		game_create: lfgevent('game_create', 20),
+		game_cancel: lfgevent('game_cancel', -20),
+		commendation: lfgevent('commendation', 5),
 		ban: lfgevent('ban', 0),
 		unban: lfgevent('unban', 0),
 		participation: lfgevent('participation', 10),
 		leaving: lfgevent('leaving', -10),
-		miss: lfgevent('miss', -10),
+		miss: lfgevent('miss', -30),
 		report: lfgevent('report', 0),
 	},
 };
