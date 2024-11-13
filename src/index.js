5d913d7c635408c32fa45d8574f3cb2dba1f3e13
Luis Lopes <josluis.lopes@gmail.com>
2024-11-13 10:40:11 +0000

    feat: use new workflow (#103)
    * Add logging (#99)
    
    * feat: use new workflow
    
    * chore: deleted old workflows
    
    * chore: remove telegram send
    
    * fix: use internal workflows
    
    * fix: workflow
    
    * chore: run on public gh nodes
    
    * chore: disable test
    
    * chore: disable test
    
    * okay
    
    * feat: add labeler
    
    * feat: add release please
    
    * chore: fix lint
    
    * Chore: chore
    
    * chore: fix build

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ client.commands = new Discord.Collection();
 // SENTRY
 const Sentry = require('@sentry/node');
 
+console.log('Starting bot...');
+
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
 	environment: process.env.NODE_ENV,
@@ -31,6 +33,7 @@ setTimeout(() => {
 		recursive('./src/commands', function(err, commandFiles) {
 			for (const file of commandFiles) {
 				const command = require(process.cwd() + '/' + file);
+				console.log('Adding command: ' + command.name);
 				// set a new item in the Collection
 				// with the key as the command name and the value as the exported module
 				client.commands.set(command.name, command);
@@ -41,19 +44,24 @@ setTimeout(() => {
 		for (const file of eventFiles) {
 			const event = require(`./events/${file}`);
 			if (event.once) {
+				console.log('Adding ONCE event: ' + event.name);
 				client.once(event.name, (...args) => event.execute(...args, client));
 			}
 			else {
+				console.log('Adding event: ' + event.name);
 				client.on(event.name, (...args) => event.execute(...args, client));
 			}
 		}
 
+		console.log('Login bot to Discord...');
 		client.login(process.env.BOT_TOKEN);
 	}
 	catch (e) {
+		console.error(e);
 		Sentry.captureException(e);
 	}
 	finally {
+		console.log('Done!');
 		transaction.finish();
 	}
 }, 99);
